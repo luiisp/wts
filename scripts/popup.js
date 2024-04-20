26f6d48c3c96161c2ff5d6cde1974ddb9db5394e
scr6w <playerzone06@gmail.com>
2024-04-20 14:55:28 -0300

    receive term in content scripts and more

---
+++ scripts/popup.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('.k-or-p');
+
+
+
+input.addEventListener('input', async () => {
+   // if (!input.value.trim() || input.value.length < 2) {
+   //     return;
+   // }
+
+    await chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        console.log('sending', input.value);
+        chrome.tabs.sendMessage(tabs[0].id, { term: input.value });
+    });
+});
