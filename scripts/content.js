3c52fafadc93f120e7d6114315b76afed95142ef
scr6w <playerzone06@gmail.com>
2024-04-20 22:27:14 -0300

    highlight terms on the page (content script)

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,54 @@
-chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
-    console.log('received', request.term); 
+const searchInPage = (term) => {
+    
+    removeAllHighlights();
+    if (!term) return;
+    const range = document.createRange();
+    const walker = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT, null, false);
 
+    let node;
+    let count = 0;
+    while (node = walker.nextNode()) {
+        const nodeText = node.nodeValue;
+        const startIndex = nodeText.toLowerCase().indexOf(term.toLowerCase());
+
+        if (startIndex >= 0) {
+            count++;
+            const highlightSpan = document.createElement('mark');
+            highlightSpan.style.backgroundColor = '#100E15';
+            highlightSpan.style.color = '#00d876';
+            highlightSpan.textContent = nodeText.substring(startIndex, startIndex + term.length);
+
+
+            const beforeText = document.createTextNode(nodeText.substring(0, startIndex));
+
+            const afterText = document.createTextNode(nodeText.substring(startIndex + term.length));
+
+            const parentNode = node.parentNode;
+            parentNode.insertBefore(beforeText, node);
+            parentNode.insertBefore(highlightSpan, node);
+            parentNode.insertBefore(afterText, node);
+
+
+            parentNode.removeChild(node);
+        }
+        
+    }
+    return count;
+}
+
+const removeAllHighlights = () => {
+    const highlighted = document.querySelectorAll('mark');
+    highlighted.forEach(mark => {
+        const parent = mark.parentNode;
+        parent.replaceChild(document.createTextNode(mark.textContent), mark);
+        parent.normalize();
+    });
+}
+
+
+chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
+    let re = searchInPage(request.term);
+    sendResponse({count: re});
 });
 
 
--- scripts/popup.js
+++ scripts/popup.js
@@ -3,12 +3,16 @@ const input = document.querySelector('.k-or-p');
 
 
 input.addEventListener('input', async () => {
-   // if (!input.value.trim() || input.value.length < 2) {
-   //     return;
-   // }
+    //if (!input.value.trim() || input.value.length < 2) {
+    //    return;
+    //}
 
     await chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         console.log('sending', input.value);
-        chrome.tabs.sendMessage(tabs[0].id, { term: input.value });
+        chrome.tabs.sendMessage(tabs[0].id, { term: input.value }, (response) => {
+            if (response.count){
+                console.log('response', response.count);
+            }
+        });
     });
 });
