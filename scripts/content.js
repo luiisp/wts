e3b139d01e79793db2a3dc1cb94d480430d2e76f
scr6w <playerzone06@gmail.com>
2024-04-21 04:19:39 -0300

    format document + change fonts

--- scripts/content.js
+++ scripts/content.js
@@ -1,92 +1,110 @@
-
 const emphasis = (e) => {
-    e.scrollIntoView({ block: 'center' }); 
-    e.style.border = '1px solid #F30A4D ';
-}
-
+  e.scrollIntoView({ block: "center" });
+  e.style.border = "1.2px solid #100E15 ";
+  e.style.backgroundColor = "#00d876";
+  e.style.color = "#100E15";
+};
 
 const searchInPage = (term) => {
-    matches = [];
+  matches = [];
 
-    if (!term) return;
-    const range = document.createRange();
-    const walker = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT, null, false);
+  if (!term) return;
+  const range = document.createRange();
+  const walker = document.createNodeIterator(
+    document.body,
+    NodeFilter.SHOW_TEXT,
+    null,
+    false
+  );
 
-    let node;
-    let firstHighlight = null;
-    let count = 0;
-    while (node = walker.nextNode()) {
-        const nodeText = node.nodeValue;
-        const startIndex = nodeText.toLowerCase().indexOf(term.toLowerCase());
+  let node;
+  let firstHighlight = null;
+  let count = 0;
+  while ((node = walker.nextNode())) {
+    const nodeText = node.nodeValue;
+    const startIndex = nodeText.toLowerCase().indexOf(term.toLowerCase());
 
-        if (startIndex >= 0) {
-            count++;
-            const highlightSpan = document.createElement('mark');
-            highlightSpan.style.backgroundColor = '#100E15';
-            highlightSpan.style.color = '#00d876';
-            highlightSpan.textContent = nodeText.substring(startIndex, startIndex + term.length);
+    if (startIndex >= 0) {
+      count++;
+      const highlightSpan = document.createElement("mark");
+      highlightSpan.style.backgroundColor = "#100E15";
+      highlightSpan.style.color = "#00d876";
+      highlightSpan.style.fontWeight = "bold";
+      highlightSpan.textContent = nodeText.substring(
+        startIndex,
+        startIndex + term.length
+      );
 
+      const beforeText = document.createTextNode(
+        nodeText.substring(0, startIndex)
+      );
 
-            const beforeText = document.createTextNode(nodeText.substring(0, startIndex));
+      const afterText = document.createTextNode(
+        nodeText.substring(startIndex + term.length)
+      );
 
-            const afterText = document.createTextNode(nodeText.substring(startIndex + term.length));
+      const parentNode = node.parentNode;
+      parentNode.insertBefore(beforeText, node);
+      parentNode.insertBefore(highlightSpan, node);
+      parentNode.insertBefore(afterText, node);
 
-            const parentNode = node.parentNode;
-            parentNode.insertBefore(beforeText, node);
-            parentNode.insertBefore(highlightSpan, node);
-            parentNode.insertBefore(afterText, node);
-
-
-            parentNode.removeChild(node);
-            matches.push(highlightSpan);
-        }
-        
+      parentNode.removeChild(node);
+      matches.push(highlightSpan);
     }
-    emphasis(matches[0])
-    
+  }
+  emphasis(matches[0]);
 
-    return {count:count, emphasisObj:{
-        actualMatch: 0,
-        maxMatchs: matches.length-1,
-        minMatchs: 0 
-    }};
-}
+  return {
+    count: count,
+    emphasisObj: {
+      actualMatch: 0,
+      maxMatchs: matches.length - 1,
+      minMatchs: 0,
+    },
+  };
+};
 
 const removeAllHighlights = () => {
-    const highlighted = document.querySelectorAll('mark');
-    highlighted.forEach(mark => {
-        const parent = mark.parentNode;
-        parent.replaceChild(document.createTextNode(mark.textContent), mark);
-        parent.normalize();
-    });
-}
+  const highlighted = document.querySelectorAll("mark");
+  highlighted.forEach((mark) => {
+    const parent = mark.parentNode;
+    parent.replaceChild(document.createTextNode(mark.textContent), mark);
+    parent.normalize();
+  });
+};
 
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log(request);
+  switch (request.type) {
+    case "analyzeDom":
+      removeAllHighlights();
+      if (request.data.term == "")
+        return sendResponse({ count: false, stopPlaceholder: true });
 
-chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
-    console.log(request);
-    switch (request.type) {
-        case 'analyzeDom':
-            removeAllHighlights();
-            if (request.data.term == '') return sendResponse({count: false,stopPlaceholder: true});
-        
-            let result = searchInPage(request.data.term);
-            sendResponse({count: result.count,stopPlaceholder: false, emphasisObj: result.emphasisObj});
-            break;
-        case 'arrowChange':
-            console.log(matches);
-            console.log(request.data.newEmphasis);
-            for (let i of [request.data.newEmphasis - 1, request.data.newEmphasis + 1]) {
-                if (matches[i]) {
-                    matches[i].style.border = 'none';
-                }
-            }
-            emphasis(matches[request.data.newEmphasis]);
+      let result = searchInPage(request.data.term);
+      sendResponse({
+        count: result.count,
+        stopPlaceholder: false,
+        emphasisObj: result.emphasisObj,
+      });
+      break;
+    case "arrowChange":
+      console.log(matches);
+      console.log(request.data.newEmphasis);
+      for (let i of [
+        request.data.newEmphasis - 1,
+        request.data.newEmphasis + 1,
+      ]) {
+        if (matches[i]) {
+          matches[i].style.border = "none";
+          matches[i].style.color = "#00d876";
+          matches[i].style.backgroundColor = "#100E15";
+        }
+      }
+      emphasis(matches[request.data.newEmphasis]);
 
-            break;
-        default:
-            break;
-    }
-
+      break;
+    default:
+      break;
+  }
 });
-
-
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,70 +1,84 @@
-const input = document.querySelector('.k-or-p');
-const resultDiv = document.querySelector('.result');
-const foundedTerms = document.querySelector('.founded-terms');
-const urlRealSearch = document.querySelector('.url-real-search');
-const downBtn = document.querySelector('.down');
-const upBtn = document.querySelector('.up');
-const btns = document.querySelector('.btns-i');
-const resultsExplorer = document.querySelector('.results-explorer');
+const input = document.querySelector(".k-or-p");
+const resultDiv = document.querySelector(".result");
+const foundedTerms = document.querySelector(".founded-terms");
+const urlRealSearch = document.querySelector(".url-real-search");
+const downBtn = document.querySelector(".down");
+const upBtn = document.querySelector(".up");
+const btns = document.querySelector(".btns-i");
+const resultsExplorer = document.querySelector(".results-explorer");
 let emphasisObj = {};
 
 const arrowChange = async (direction) => {
-    const isunique = emphasisObj.actualMatch === emphasisObj.maxMatchs && emphasisObj.actualMatch === emphasisObj.minMatchs;
-    if (!direction || !emphasisObj || isunique) return;
-    emphasisObj.actualMatch += (direction === 'down') ? 1 : -1;
-    if (emphasisObj.actualMatch < emphasisObj.minMatchs) {
-        emphasisObj.actualMatch = emphasisObj.maxMatchs;
-    }else if (emphasisObj.actualMatch > emphasisObj.maxMatchs) {
-        emphasisObj.actualMatch = emphasisObj.minMatchs;
-    }
+  const isunique =
+    emphasisObj.actualMatch === emphasisObj.maxMatchs &&
+    emphasisObj.actualMatch === emphasisObj.minMatchs;
+  if (!direction || !emphasisObj || isunique) return;
+  emphasisObj.actualMatch += direction === "down" ? 1 : -1;
+  if (emphasisObj.actualMatch < emphasisObj.minMatchs) {
+    emphasisObj.actualMatch = emphasisObj.maxMatchs;
+  } else if (emphasisObj.actualMatch > emphasisObj.maxMatchs) {
+    emphasisObj.actualMatch = emphasisObj.minMatchs;
+  }
 
-    await chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, {type: 'arrowChange' , data: {newEmphasis:emphasisObj.actualMatch} }, (response) => {
-            //if (response.stopPlaceholder) {
-            //    return;
-            //}
-            resultsExplorer.textContent = `Result ${emphasisObj.actualMatch + 1} of ${emphasisObj.maxMatchs}`;
-            console.log(response);
-        });
-    });
-    
+  await chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { type: "arrowChange", data: { newEmphasis: emphasisObj.actualMatch } },
+      (response) => {
+        //if (response.stopPlaceholder) {
+        //    return;
+        //}
+        resultsExplorer.textContent = `Result ${
+          emphasisObj.actualMatch + 1
+        } of ${emphasisObj.maxMatchs + 1}`;
+        console.log(response);
+      }
+    );
+  });
 };
-downBtn.addEventListener('click', async () => arrowChange('down'));
-upBtn.addEventListener('click', async () => arrowChange('up'))
-input.addEventListener('input', async () => {
-    await chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, {type: 'analyzeDom' , data: {term: input.value} }, (response) => {
-            if (response.stopPlaceholder) {
-                resultDiv.style.display = 'none';
-                chrome.action.setBadgeText({
-                    text: '',
-                  });
-                return;
-            }
+downBtn.addEventListener("click", async () => arrowChange("down"));
+upBtn.addEventListener("click", async () => arrowChange("up"));
+input.addEventListener("input", async () => {
+  await chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { type: "analyzeDom", data: { term: input.value } },
+      (response) => {
+        if (response.stopPlaceholder) {
+          resultDiv.style.display = "none";
+          chrome.action.setBadgeText({
+            text: "",
+          });
+          return;
+        }
 
-            if (response.count) {
-                let count = response.count;
-                let lessTwo = count < 2;
-                if (resultDiv.style.display === 'none') {
-                    resultDiv.style.display = '';
-                }
-                emphasisObj = response.emphasisObj;
-                if (emphasisObj.actualMatch === emphasisObj.maxMatchs) {
-                    resultsExplorer.style.display = 'none';
-                    btns.style.display = 'none';
-                }else{
-                    resultsExplorer.style.display = '';
-                    btns.style.display = '';
-                    resultsExplorer.textContent = `Result ${emphasisObj.actualMatch + 1} of ${emphasisObj.maxMatchs + 1}`;
-                }
-                foundedTerms.textContent = `${count} ${ lessTwo ? 'result' : 'results' } found`;
-                urlRealSearch.textContent = `Search for ${input.value} on internet`;
-                urlRealSearch.href = `https://www.google.com/search?q=${input.value}`;
-                chrome.action.setBadgeText({
-                    text: count.toString(),
-                  });
-
-            }
-        });
-    });
+        if (response.count) {
+          let count = response.count;
+          let lessTwo = count < 2;
+          if (resultDiv.style.display === "none") {
+            resultDiv.style.display = "";
+          }
+          emphasisObj = response.emphasisObj;
+          if (emphasisObj.actualMatch === emphasisObj.maxMatchs) {
+            resultsExplorer.style.display = "none";
+            btns.style.display = "none";
+          } else {
+            resultsExplorer.style.display = "";
+            btns.style.display = "";
+            resultsExplorer.textContent = `Result ${
+              emphasisObj.actualMatch + 1
+            } of ${emphasisObj.maxMatchs + 1}`;
+          }
+          foundedTerms.textContent = `${count} ${
+            lessTwo ? "result" : "results"
+          } found`;
+          urlRealSearch.textContent = `Search for ${input.value} on internet`;
+          urlRealSearch.href = `https://www.google.com/search?q=${input.value}`;
+          chrome.action.setBadgeText({
+            text: count.toString(),
+          });
+        }
+      }
+    );
+  });
 });
